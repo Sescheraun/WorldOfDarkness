eeebc2dccde450f6182c1a55187797eb08e235c6
Sescheraun <rcollier@madisoncollege.edu>
2018-12-13 11:35:31 -0600

    The post request is now being correctly recieved and responded to.

--- src/main/java/sescheraun/worldoffuturedarkness/controler/SubCritterCRUD.java
+++ src/main/java/sescheraun/worldoffuturedarkness/controler/SubCritterCRUD.java
@@ -1,40 +1,61 @@
 package sescheraun.worldoffuturedarkness.controler;
 import org.apache.logging.log4j.*;
 import sescheraun.worldoffuturedarkness.generator.Critter;
+import sescheraun.worldoffuturedarkness.generator.SubCritter;
 import sescheraun.worldoffuturedarkness.persistance.GenericDAO;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(
-        urlPatterns = {"/prepSubCritterForm"}
+        urlPatterns = {"/subCritterCRUD"}
 )
 
 
 public class SubCritterCRUD extends HttpServlet {
+    private static GenericDAO critterDAO = new GenericDAO(Critter.class);
+    private static GenericDAO subCritterDAO = new GenericDAO(SubCritter.class);
+    private static String RETURN_TO = "/admin/subCritters.jsp";
 
     private final Logger logger = LogManager.getLogger(this.getClass());
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //place holder
+        String response = "You have reached the doGet method";
+        logger.info(response);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //place holder
+        String response = "You have reached the doPost method";
+        logger.info(response);
+
+
+        logger.info(req.getParameter("critter"));
+        logger.info(req.getParameter("subCritterName"));
+        logger.info(req.getParameter("category"));
+        logger.info(req.getParameter("firstAdvantage"));
+        logger.info(req.getParameter("secondAdvantage"));
+        logger.info(req.getParameter("flaw"));
+
+        resp.setContentType("text/plain");
+        resp.getWriter().write("Post Recieved.");
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //place holder
+        String response = "You have reached the doPut method";
+        logger.info(response);
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //place holder
+        String response = "You have reached the doDelete method";
+        logger.info(response);
     }
+
 }
--- src/main/webapp/scripts/scripts.js
+++ src/main/webapp/scripts/scripts.js
@@ -1,5 +1,75 @@
 $(document).ready( () => {
+    var CORE_LOCATION = "http://localhost:8080/WorldOfFutureDarkness/";
 
+    backgroundFlip();
+
+
+    centerTheThing();
+
+    /*********************************************************************
+     **                       Add New Subcritter                        **
+     ** This function is called from admin/subCritter.jsp and sends a   **
+     ** new subcritter to the subCritterCrud servelet to be added to    **
+     ** to the data base.                                               **
+     **                                                                 **
+     ** When i have more time, and the rest of the application is done, **
+     ** I would like to refactor this to user observables.              **
+     *********************************************************************/
+    $("#subCritterSubmit").on("click", () => {
+
+        let critter         = $("#allowed :selected").prop("id");
+        let subCritterName      = $("#subCritterName").val();
+        let category        = $("#category").val();
+        let firstAdvantage  = $("#firstAdvantage").val();
+        let secondAdvantage = $("#secondAdvantage").val();
+        let flaw            = $("#flaw").val();
+
+        subCritter = {"critter": critter
+                 , "subCritterName": subCritterName
+                   , "category": category
+             , "firstAdvantage": firstAdvantage
+            , "secondAdvantage": secondAdvantage
+                       , "flaw": flaw
+        }
+
+        //Make it standard postData for now.
+        postData = `critter=${critter}`
+            + `&subCritterName=${subCritterName}`
+            + `&category=${category}`
+            + `&firstAdvantage=${firstAdvantage}`
+            + `&secondAdvantage=${secondAdvantage}`
+            + `&flaw=${flaw}`;
+
+        console.log(subCritter);
+
+        $.ajax({
+            url:CORE_LOCATION + "subCritterCRUD"
+            , method: "POST"
+            , data: postData
+            , dataType: "TEXT"
+            , success: function(responseText) {
+                // let data = JSON.parse(responseText);
+                console.log(responseText);
+            }
+            , error:function(xhr, status, error) {
+                console.log("ERROR:");
+                console.log(xhr.responseText);
+                console.log(postData);
+                console.log(status);
+                console.log(error);
+
+                //TODO: put some code here to put the failure on the webpage
+            }
+        })
+    });
+
+})
+
+
+/*********************************************************************
+ **                         Background flip                         **
+ *********************************************************************/
+backgroundFlip = () => {
     var backgrounds = [
         "midnightMeeting.png"
         , "guyOnGargoyle.jpg"
@@ -19,7 +89,12 @@ $(document).ready( () => {
 
     container.backgroundImage = image;
     container.backgroundRepeat = "noRepeat";
+}
 
+/*********************************************************************
+ **                          Center Things                          **
+ *********************************************************************/
+centerTheThing = () => {
     var element = document.getElementById("content");
     element.style.position = "absolute";
 
@@ -28,27 +103,4 @@ $(document).ready( () => {
 
     var vertical = (window.innerHeight / 2) - (element.offsetHeight / 2);
     element.style.top = vertical + 'px';
-
-
-    $("#subCritterSubmit").on("click", () => {
-
-        let critter         = $("#allowed :selected").prop("id");
-        let subCritter      = $("#subCritterName").val();
-        let category        = $("#category").val();
-        let firstAdvantage  = $("#firstAdvantage").val();
-        let secondAdvantage = $("#secondAdvantage").val();
-        let flaw            = $("#flaw").val();
-
-        subCritter = {"critter": critter
-                 , "subCritter": subCritter
-                   , "category": category
-             , "firstAdvantage": firstAdvantage
-            , "secondAdvantage": secondAdvantage
-                       , "flaw": flaw
-        }
-        console.log(subCritter);
-
-        // lookup @formdata
-    });
-
-})
+}
